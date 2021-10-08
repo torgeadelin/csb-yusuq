d5c00b284c8312da49104a31a9e5ef8bbcf0d376
Code Sandbox <hello@codesandbox.io>
2021-10-08 10:20:49 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b886c9dbe6ac758444bd7b7bf1320bff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a00d0fc30ff9b08ad75d9449d876eb1",
+    "url": "https://.github.io/csb-yusuq/index.html"
+  },
+  {
+    "revision": "8e82695c9f5e444cab68",
+    "url": "https://.github.io/csb-yusuq/static/js/2.b8e28b1d.chunk.js"
+  },
+  {
+    "revision": "2b1bf4c5f6156582acdb3d33a1b72617",
+    "url": "https://.github.io/csb-yusuq/static/js/2.b8e28b1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b8e03825653c17935f4",
+    "url": "https://.github.io/csb-yusuq/static/js/main.64095cac.chunk.js"
+  },
+  {
+    "revision": "f9ea004ec5e6cdd90339",
+    "url": "https://.github.io/csb-yusuq/static/js/runtime-main.4e409b89.js"
+  }
+]);
